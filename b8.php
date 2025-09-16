b1272ac14c9b361e893a8f45a617f1fad24ea797
Bá Đông Lê <badongle.2006@gmail.com>
2025-09-16 10:32:27 +0700

    ver1

---
+++ b8.php
@@ -0,0 +1,9 @@
+<?php
+ $color = array('white', 'green', 'red', 'blue', 'black');
+ foreach ($color as $value) {
+    echo "$value <br>";
+ }
+ for($i = 0; $i < count($color); $i++){
+    echo $color[$i] . "<br>";
+ }  
+?>
