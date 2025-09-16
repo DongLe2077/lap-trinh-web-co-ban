aa0a29f42a7c94dd37e48096b2fb8de82375bb99
Bá Đông Lê <badongle.2006@gmail.com>
2025-09-16 09:17:05 +0700

    ver1

---
+++ b5.php
@@ -0,0 +1,12 @@
+<?php
+    $a = 4;
+    $b= 8;
+   $ss_bang = $a == $b;
+   echo "So sanh bang: $ss_bang <br> ";
+
+   $ss_khac = $a != $b;
+    echo "So sanh khac: $ss_khac <br> ";
+
+    $ss_lon_hon = $a > $b;
+    echo "So sanh lon hon: $ss_lon_hon <br> ";
+?>
